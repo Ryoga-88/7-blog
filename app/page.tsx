f1625557ceb28487975b9e5a2a30759ffd47c6df
Ryoga-88 <hryoga@gmail.com>
2025-04-06 21:25:49 +0900

    md メンバー紹介ページの追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { getProjectCards } from "../lib/cardcontents";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import Card from "@/components/card";
+import Image from "next/image";
 
 export default async function Home() {
   const contents = await getSortedContents();
@@ -26,13 +27,11 @@ export default async function Home() {
               {title && <h1 className="text-3xl font-bold">{title}</h1>}
               {subtitle && <h1 className="text-md mb-8">{subtitle}</h1>}
 
-              {/* mdファイルの内容を表示 */}
               <div
                 className="font-medium prose prose-img:mx-auto prose-img:max-w-full prose-img:rounded-md prose-img:shadow-sm prose-img:mb-8 prose-strong:text-black max-w-none"
                 dangerouslySetInnerHTML={{ __html: html }}
               />
 
-              {/* titleがProjectの場合のみ、カードを追加表示 */}
               {title === "Project" && (
                 <div className="grid grid-cols-1 gap-2">
                   {projectCards.map((card, cardIndex) => (
@@ -44,6 +43,55 @@ export default async function Home() {
                       link={card.link}
                     />
                   ))}
+                </div>
+              )}
+
+              {title === "Members" && (
+                <div className="w-full">
+                  <div className="flex md:flex-row flex-col justify-center pb-8 items-center md:justify-start md:items-start md:pr-4 ">
+                    <div className="md:w-full max-w-[114px]">
+                      <Image
+                        src="/img/KatsuhiroHonda.jpg"
+                        alt="human"
+                        width={114}
+                        height={152}
+                        className="object-cover aspect-4/3  min-w-[114px] min-h-[152px] rounded-sm"
+                      />
+                    </div>
+                    <div>
+                      <div className="text-base text-left dark:text-white mt-6 md:mt-0 md:ml-8">
+                        <div className="text-xl font-bold mb-2">
+                          本多 克宏 (Katsuhiro Honda)
+                        </div>
+                        <div>
+                          大学院情報学研究科 基幹情報学専攻 教授<br></br>
+                          専門はソフトコンピューティングとデータマイニングで、特にファジィクラスタリングを中心とした研究に取り組んでいる。多変量データ解析や推薦システムの開発を通じて、ユーザの意思決定を支援するシステム構築に注力。研究室では学生とともに、人に優しい情報化社会の実現を目指している。
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex md:flex-row flex-col justify-center py-8 items-center md:justify-start md:items-start md:pr-4 border-t-1 border-gray-400">
+                    <div className="md:w-full max-w-[114px]">
+                      <Image
+                        src="/img/SeikiUbukata.jpg"
+                        alt="human"
+                        width={114}
+                        height={152}
+                        className="object-cover aspect-4/3  min-w-[114px] min-h-[152px] rounded-sm"
+                      />
+                    </div>
+                    <div>
+                      <div className="text-base text-left dark:text-white mt-6 md:mt-0 md:ml-8">
+                        <div className="text-xl font-bold mb-2">
+                          生方 誠希 (Seiki Ubukata)
+                        </div>
+                        <div>
+                          大学院情報学研究科 基幹情報学専攻 准教授<br></br>
+                          ラフクラスタリング技術を専門とし、新たなクラスタリングアルゴリズムの開発と協調フィルタリングへの応用研究を進めている。データ解析の理論と実践の両面から研究に取り組み、国内外での学術発表や受賞歴も多数。学生と共に最先端の知能情報技術の探求に日々励んでいる。
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               )}
             </div>
--- components/header.tsx
+++ components/header.tsx
@@ -16,7 +16,7 @@ export default function Header({ navItems = [] }: HeaderProps) {
   return (
     <>
       <div className="w-full sticky top-0 z-50 bg-white py-2">
-        <div className="flex items-center justify-between mx-8 md:mx-24 flex-wrap">
+        <div className="flex items-center justify-between mx-4 md:mx-24 flex-wrap">
           <Logo />
           <Nav items={navItems} />
         </div>
